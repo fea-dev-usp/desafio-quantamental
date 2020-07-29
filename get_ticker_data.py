920184bf60a02a76443548e509bca75707212fdd
João Victor A. E <56171018+joaovae@users.noreply.github.com>
2020-07-29 19:03:56 -0300

    Add files via upload

--- get_ticker_data.py
+++ get_ticker_data.py
@@ -7,22 +7,17 @@ Created on Sun Jul  5 13:20:27 2020
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import time
 import pandas as pd
+import pyfolio as pf
 
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 
-creds = ServiceAccountCredentials.from_json_keyfile_name('token.json', scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name(r'C:\Users\JOAO VICTOR\Desktop\quant-trade\token.json', scope)
 client = gspread.authorize(creds)
 
 sheet = client.open('Quant').sheet1
-
-data = sheet.get_all_records()
-
-value_NA = sheet.row_values(1)
-
-z = 0
-x = 2
 
 ticker2016 = ['TIET11','BTOW3','BBAS3', 'BBDC3', 'BRKM3', 'BRFS3', 'CCRO3', 'CMIG3',
           'CESP3', 'CIEL3', 'CPLE3', 'CPFE3', 'DTEX3', 'ECOR3', 'ENBR3', 'ELET3',
@@ -30,14 +25,37 @@ ticker2016 = ['TIET11','BTOW3','BBAS3', 'BBDC3', 'BRKM3', 'BRFS3', 'CCRO3', 'CMI
           'LAME4', 'LIGT3', 'OIBR4', 'LREN3', 'SANB11', 'SULA11', 'VIVT4',
           'TIMP3', 'EGIE3', 'WEGE3' ]
 
-df = pd.DataFrame()
+def get_ticker_df(lista_ticker):
+
+    dic_price = {}
+
+    def get_ticker_prices(ticker):
+
+    sheet.update_cell(1, 2, '{}'.format(ticker))
+    time.sleep(4)
+    price_close = sheet.col_values(5)[2:]
+    return price_close
+
+    for i in lista_ticker:
+
+        prices = get_ticker_price(i)
+        dic_price['{}'.format(i)] = prices
+
+    df = pd.DataFrame.from_dict(dic_price, orient = 'index')
+    df = df.transpose().astype(float)
+    return df
 
 
-for i in sheet.row_values(1):
 
-    if i in ticker2016:
+'''
+#Cálculo de retorno por uma ação
+retorno = df['WEGE3'].pct_change()
 
-        df['Ação {}'.format(x)] = sheet.col_values(x)
+retorno_acumulado = (1 + retorno).cumprod()
+retorno_acumulado.iloc[0] = 1
 
-    x += 1
+carteira = 10000*retorno_acumulado
+carteira['saldo'] = carteira.sum(axis=1)
+carteira['retorno'] = carteira['saldo'].pct_change()
+'''
 
