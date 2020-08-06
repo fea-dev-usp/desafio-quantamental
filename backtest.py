508eb94ce2b4878f2ec665099e7879cb8595edfc
joaovae <joao.victor.a.e@usp.br>
2020-08-05 22:24:56 -0300

    update backtest

--- backtest.py
+++ backtest.py
@@ -9,31 +9,38 @@ import pandas_datareader.data as web
 import pandas as pd
 import pyfolio as pf
 import warnings
+import numpy as np
 
-ticker2016 = ['TIET11.SA','BTOW3.SA','BBAS3.SA', 'BBDC3.SA', 'BRKM3.SA', 'BRFS3.SA', 'CCRO3.SA', 'CMIG3.SA',
-          'CESP3.SA', 'CIEL3.SA', 'CPLE3.SA', 'CPFE3.SA', 'DTEX3.SA', 'ECOR3.SA', 'ENBR3.SA', 'ELET3.SA',
-          'EMBR3.SA', 'EVEN3.SA', 'SUZB3.SA', 'FLRY3.SA', 'ITSA4.SA', 'ITUB3.SA', 'KLBN11.SA',
-          'LAME4.SA', 'LIGT3.SA', 'OIBR4.SA', 'LREN3.SA', 'SANB11.SA', 'SULA11.SA', 'VIVT4.SA',
-          'TIMP3.SA', 'EGIE3.SA', 'WEGE3.SA', 'SUZB3']
+ticker2016 = ['TIET11.SA','BTOW3.SA','BBAS3.SA']
 dados_yahoo = web.get_data_yahoo(ticker2016, start='2016-01-01', end='2017-01-01')['Adj Close']
 
+
+ibov2016 = ['^BVSP']
+dados_ibov = web.get_data_yahoo(ibov2016, start='2016-01-01', end='2017-01-01')['Adj Close']
+
+lista = [1.0, 0.0, 0.00]
+
 def backtest(df_ticker, df_ibov, lista_pct):
-    retorno = df_ticker
+
+    lista_pct = np.array(lista_pct).reshape(1, 3)
+
+    retorno = df_ticker.pct_change()
     retorno_acumulado = (1 + retorno).cumprod()
     retorno_acumulado.iloc[0] = 1
 
-    carteira = 10000*lista_pct*retorno_acumulado
-
+    carteira = retorno_acumulado.multiply(lista, axis = 1)
     carteira['saldo'] = carteira.sum(axis=1)
     carteira['retorno'] = carteira['saldo'].pct_change()
 
-    retorno_ibov = df_ibov
+    retorno_ibov = df_ibov.pct_change()
     retorno_acumulado_ibov = (1 + retorno_ibov).cumprod()
     retorno_acumulado_ibov.iloc[0] = 1
 
-    carteira_ibov = 10000*retorno_acumulado_ibov
+    carteira_ibov = retorno_acumulado_ibov
 
     carteira_ibov['saldo'] = carteira_ibov.sum(axis=1)
     carteira_ibov['retorno'] = carteira_ibov['saldo'].pct_change()
 
     return pf.create_full_tear_sheet(carteira['retorno'], benchmark_rets=carteira_ibov['retorno'])
+
+backtest(dados_yahoo, dados_ibov, lista)
